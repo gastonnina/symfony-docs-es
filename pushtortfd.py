a3fe6ea44a84e1915ecd20a71f8562dadea07c86
Carlos Zuniga <carlos.zun@gmail.com>
2011-03-23 13:29:35 -0500

    added script to upload built html

---
+++ pushtortfd.py
@@ -0,0 +1,54 @@
+import os
+import zipfile
+import mechanize
+import netrc
+
+######## config ########
+
+repo = 'test-sf-doc-es'
+
+######## generar zip ########
+target_dir = os.path.join(os.path.realpath('.'), '_build', 'html')
+
+buildname = 'htmlbuild.zip'
+
+buildzip = zipfile.ZipFile(buildname, 'w', zipfile.ZIP_DEFLATED)
+rootlen = len(target_dir) + 1
+for base, dirs, files in os.walk(target_dir):
+    for file in files:
+        fn = os.path.join(base, file)
+        buildzip.write(fn, fn[rootlen:])
+buildzip.close()
+
+######## abrir navegador ########
+
+b = mechanize.Browser()
+b.open('http://readthedocs.org/accounts/login/')
+
+######## login ########
+# el form de login es el segundo
+f = [i for i in b.forms()][1]
+
+n = netrc.netrc()
+
+f['username'] = n.hosts['readthedocs.org'][0]
+f['password'] = n.hosts['readthedocs.org'][2]
+
+b.form = f
+b.submit()
+
+
+######## submit zip file ########
+b.open('http://readthedocs.org/dashboard/upload_html/' + repo)
+
+f = [i for i in b.forms()][1]
+
+b.form = f
+
+b.form.add_file(open(buildname), filename=os.path.basename(buildname), name='content')
+
+b.form['overwrite'] = ['on']
+
+b.submit()
+
+
